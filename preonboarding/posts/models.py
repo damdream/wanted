f9f7526dc39bf1081a36ae4effa17148609e7d0b
damdream <possible_11@naver.com>
2021-10-26 15:21:31 +0900

    Add : Post View 추가

--- preonboarding/posts/models.py
+++ preonboarding/posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     title           = models.CharField(max_length=45)
     comment         = models.CharField(max_length=2000, null=True)
     created_at      = models.DateTimeField(auto_now_add=True)
-	
+    updated_at      = models.DateTimeField(auto_now=True, null=True)
+
     class Meta:
 	    db_table = 'posts'
--- preonboarding/posts/views.py
+++ preonboarding/posts/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse, request
+from django.views     import View
+
+from users.models     import User
+from posts.models     import Post
+from users.utils      import login_decorator
+
+class PostingView(View):
+    @login_decorator    
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+
+            if data["title"] == "" :
+                return JsonResponse({"MESSAGE":"NULL_REVIEWS"}, status = 400)
+
+            Post.objects.create( 
+                user            = request.user,
+                name            = data["name"],
+                title           = data["title"],
+                comment         = data["comment"],
+            )
+
+            return JsonResponse({"MESSAGE":"SUCCESS"}, status = 201)
+
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status = 400)    
+
+    def get(self,request, post_id):
+        posts = Post.objects.all()
+
+        result = [
+            {
+                "user"            : post.user.id,
+                "name"            : post.name ,
+                "created_at"      : post.created_at,
+                "title"           : post.title,
+                "comment"         : post.comment,
+
+            } for post in posts]
+
+        return JsonResponse ({"result": result}, status = 200)
+
+    @login_decorator
+    def delete (self,request,post_id):
+
+        if not Post.objects.filter(id=post_id, user = request.user).exists():
+            return JsonResponse({"MESSAGE": "NO_REVIEWS"}, status = 400)
+
+        Post.objects.filter(id=post_id).delete()
+
+        return JsonResponse ({"MESSAGE" : "DELETE"}, status = 204)    
+
+    @login_decorator
+    def patch (self,request, post_id):
+        try:
+            data = json.loads(request.body)
+
+            if not Post.objects.filter(id=post_id, user = request.user).exists():
+              return JsonResponse({"MESSAGE": "NO_REVIEWS"}, status = 400)
+    
+            Post.objects.filter(id=post_id).update(title= data["title"], comment= data["comment"])
+            return JsonResponse ({"MESSAGE" : "UPDATE"}, status = 200)
+
+        except KeyError: 
+            return JsonResponse ({"MESSAGE":"KEY_ERROR"},status = 400)
